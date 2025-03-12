d786493fe414c903380b46b5bf787644bc6a009d
iamanandri <irfananandri@gmail.com>
2025-03-13 06:46:22 +0700

    Added closures with increment values

--- learningFunctions/main.go
+++ learningFunctions/main.go
@@ -3,12 +3,33 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("=== Average Function ===")
 	//Series of numbers
-	numbers := []float64{98, 93, 77, 82, 83}
-
+	numbers := []float64{98, 93, 77, 82, 83}	
 	//average := average(numbers)
 
-	fmt.Printf("The average is: %f", average(numbers))
+	fmt.Printf("The average is: %f\n", average(numbers))
+
+	//Returning Multiple Types
+	fmt.Println("=== Returning Multiple Types ===")
+	fmt.Println(returningMultipleTypes())
+
+	//Variadic Function
+	fmt.Println("=== Add Three Numbers ===")
+	vfInt := []int{5,5,5,5,5}
+	fmt.Println(addThreeNumbers(vfInt...))
+
+	//Closures
+	fmt.Println("=== Closures with Increment Values ===")
+	incrementValue := 0
+	incrementFunc := func() int {
+		incrementValue++
+		return incrementValue
+	}
+
+	for i := 0; i < 5; i++{
+		println(incrementFunc())
+	}
 }
 
 func average(x []float64) float64 {
@@ -20,3 +41,17 @@ func average(x []float64) float64 {
 
 	return (total / float64(len(x)))
 }
+
+func returningMultipleTypes() (int, string, float64){
+	return 10, "Lionel Messi", 98.98
+}
+
+func addThreeNumbers(numbers ...int) int {
+	var total int
+	for _, value := range numbers {
+		total += value
+	}
+
+	return total
+}
+
