3aa0b00769c5c45ec66d1cef4af2f0d8644e5efc
iamanandri <irfananandri@gmail.com>
2025-03-13 06:04:51 +0700

    Added and coded Learning Functions

---
+++ learningFunctions/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Series of numbers
+	numbers := []float64{98, 93, 77, 82, 83}
+
+	average := average(numbers)
+
+	fmt.Printf("The average is: %f", average)
+}
+
+func average(x []float64) float64 {
+	var total float64
+
+	for i := 0; i < len(x); i++ {
+		total += x[i]
+	}
+
+	return (total / 5)
+}
